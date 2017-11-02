428bd1114c5b2149122a3169f6b4fa170b3c239c
thmihov <33295944+thmihov@users.noreply.github.com>
2017-11-02 13:35:49 +0200

    Add files via upload

---
+++ OW_Low_Level_Functions.c
@@ -0,0 +1,155 @@
+/**************************************************/
+/* Some low level functions,like pulling line low */
+/* setting line high (idle state),Write 1,Write 0 */
+/* reading single bit,reading single byte         */
+/* writing single byte and etc.                   */
+/**************************************************/
+
+#include "OW_Port_Def.h"
+#include <avr/io.h>
+#include <util/delay.h>
+
+/**************************************************/
+	     /* Pulls DQ line low */
+
+void OW_LOW()
+{
+	DQDDR |=(1<<DQPIN);	//Sets the DQ pin as output
+	DQPORT&=~(1<<DQPIN);	//Sets the DQ pin low
+
+}
+
+/**************************************************/
+       /* Sets DQ line at idle state (high)*/
+
+void OW_IDLE()
+{
+	DQDDR &=~(1<<DQPIN);	//Sets the DQ pin as input.Because of the external  pull-up resistor,this puts the DQ line high (idle)
+}
+
+
+/**************************************************/
+/* Write 1 time slot */
+/* Pull DQ low,wait 1us (1-15us) and release.*/
+/* Wait another 60us because the time slot cannot be less than 60us -> Tlow+Tidle >= 60us */
+	
+void OW_WR1()
+{
+	OW_LOW();	//Pull DQ low
+	_delay_us(6);	//Wait 1 us
+	OW_IDLE();	//Release DQ
+	_delay_us(64);	//Wait 60us
+}
+
+
+/**************************************************/
+/* Write 0 time slot */
+/* Pull DQ low,wait min 60us (one time slot) and release */
+/* Wait another 2us because the time slot cannot be less than 60us ->Tlow+Tidle >= 60us */
+
+void OW_WR0()
+{
+	OW_LOW();	//Pull DQ low
+	_delay_us(60);	//Wait 60us
+	OW_IDLE();	//Release DQ
+	_delay_us(10);	//Wait 2us
+}
+
+/**************************************************/
+/* Read single bit from slave*/
+/* Pull DQ low,wait min. 1us than relese.Wait 15uS and read the result.*/
+/* Wait about 40us (min. time slot is 60uS) to complete the time slot */
+
+unsigned char OW_READB()
+{
+	unsigned char res=0;	//Result var
+	OW_LOW();	//Pull DQ low
+	_delay_us(6);	//Wait 5us
+	OW_IDLE();	//Release
+	_delay_us(9);	//Wait 15us
+	
+	if (!(PINB&(1<<0)))	//Read line
+	{
+		res=0;	//Slave sends 0
+	}
+	else
+	{
+		res=1;	//Slave sends 1
+	}
+	_delay_us(55);	//Wait 40us to complete the minimum time slot
+	return res;
+
+}
+
+
+/**************************************************/
+/*Read single byte from slave*/
+/*Calls 8 times 1W_READB(),stores the results in array */
+/*and converts them into single byte */
+
+unsigned char OW_READBYTE()
+{
+	 unsigned char i=0;
+	 unsigned char tarr[8];
+	 unsigned char data=0;
+	 while (i<8)
+	 {
+	 	tarr[i]=OW_READB();
+		i++;
+	 }
+	 i=0;
+	 while(i<8)
+	 {
+	 	if (tarr[i])
+		{
+			data+=(1<<i);
+		}
+		i++;
+	 }
+		return data;
+}
+
+/**************************************************/
+/* Write single byte to the slave*/
+/* Check every bit of the number and calls the corresponding function-Write 1 ot Write 0 */
+
+void OW_WRITE(unsigned char data)
+{
+	unsigned char i=0;	//Counter
+	unsigned char res;	//Result var
+	while (i<8)
+	{
+	res=(data&(1<<0));
+	if (res==1)
+	OW_WR1();
+	if (res==0)
+	OW_WR0();
+	data=data>>1;
+	i++;
+	}
+}
+
+/**************************************************/
+/* Reset condition */
+/* Pull DQ low for min.480uS,than release it.Wait 60us (15-60uS) and then read the DQ line. */
+/* If the line is low,than there is at least one device (return 1) */
+/* if the line is high there is no device or there is malfunction - short circuit or so (return 0)*/
+
+unsigned char OW_RESET()
+{
+	unsigned char res=0; //Variable for the result
+	OW_LOW();	//Pull the line LOW
+	_delay_us(480);	//Wait 480us
+	OW_IDLE();	//Release the line
+	_delay_us(70);	//WAit 60uS
+	if (!(DQIN&(1<<DQPIN)))	//Read the line
+	{
+		res=1;	//If the line is low,there is a device (return 1)
+	}
+	else
+	{
+		res=0;	//If the line is high trehe is no device (return 0)
+	}
+	_delay_us(410);	//Wait (datasheet)
+	return res;
+}
---
+++ OW_Port_Def.h
@@ -0,0 +1,8 @@
+/*****************************************************************/
+/* Port definitions.Change if needed.Default PortB,pin 0 is used */
+/*****************************************************************/
+
+#define DQPORT PORTB	        //DQ line port
+#define DQPIN 0			//DQ line pin
+#define DQDDR DDRB		//DQ line ddr
+#define DQIN PINB		//DQ line input
